69cccbb51a95520a08d7159f2cdd24802694372d
Sunil kumar.S <sk6001726@gmail.com>
2026-02-18 22:03:19 +0530

    first day commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './Navbar'
+import Navbar from './Navbar/Navbar'
 import Course from './Course/Course'
 import Footer from './Footer'
 
