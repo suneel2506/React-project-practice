afcc2e22ed51f0ca5d5dfadd2697da6e91a73673
Sunil kumar.S <sk6001726@gmail.com>
2026-03-03 20:07:50 +0530

    6th day 2nd

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -5,19 +5,24 @@ function CourseList() {
   const [courses,setCourses] =useState(null);
   //  courses.sort((x,y) => y.price - x.price)
   //  const vfmCourses = courses.filter((course) => course.price<250)
+ 
   useEffect(() =>{
-    
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch("http://localhost:3000/courses")
     .then((responce)=>{
       console.log(responce);
       return responce.json();
-    }).then((data) =>console.log(data))
-  });
+    }).then(data =>setCourses(data));
+  },[]);
 
   function handleDelete(id){
     const newCourses = courses.filter((course) => course.id != id);
     setCourses(newCourses); 
   }
+
+    if (!courses){
+      return <></>
+    } 
+
   const coursesList = courses.map((course) => (
     <Course
       key={course.id}
